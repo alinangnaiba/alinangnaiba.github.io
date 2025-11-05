6d6079fb36d03d06f4e5504efac59d9615165d82
lintong <60266082+alinangnaiba@users.noreply.github.com>
2025-11-05 16:19:13 +0000

    extract to component classes

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,4 +1,4 @@
-import type { Experience } from './types';
+import type { Experience } from "./types";
 
 export const experience: Experience[] = [
   {
@@ -14,8 +14,8 @@ export const experience: Experience[] = [
       "Reviewed pull requests to ensure code quality and adherence to best practices.",
       "Collaborated with team members on broader project specifications and feature integration.",
       "Contributed to the maintenance and support of the legacy system (C#, VB, ASP.NET Web Forms).",
-      "Identified, diagnosed, and resolved bugs and exceptions across both new and legacy systems as reported by the support team."
-    ]
+      "Identified, diagnosed, and resolved bugs and exceptions across both new and legacy systems as reported by the support team.",
+    ],
   },
   {
     title: "Senior Software Developer",
@@ -28,29 +28,31 @@ export const experience: Experience[] = [
       "Conducted testing of code functionalities across multiple environments to ensure compatibility and performance.",
       "Identified, diagnosed, and resolved bugs and exceptions reported by the support team, improving system reliability.",
       "Collaborated with team members to define, discuss, and plan the development of new features and system updates.",
-      "Monitored application logs after releases to proactively detect and resolve exceptions in production environments."
-    ]
+      "Monitored application logs after releases to proactively detect and resolve exceptions in production environments.",
+    ],
   },
   {
     title: "Senior Software Developer",
     company: "Avensia Philippines Inc.",
     location: "Cebu City, Cebu, Philippines",
     period: "September 2019 to May 2022",
-    technologies: ".NET Framework, .NET Core, WCF, inRiver PIM, Riversand Master Data Management, Azure Event Hub, Azure Service Bus, Azure Blob Storage, MySQL, Jira, Git, GitHub",
+    technologies:
+      ".NET Framework, .NET Core, WCF, inRiver PIM, Riversand Master Data Management, Azure Event Hub, Azure Service Bus, Azure Blob Storage, MySQL, Jira, Git, GitHub",
     responsibilities: [
       "Designed and implemented new features using a Product Information Management platform to enhance data management capabilities.",
       "Conducted performance and functionality testing across different environments using client-supplied data, ensuring robustness and reliability.",
       "Developed middleware solutions to facilitate smooth data migration from legacy client systems to new platforms.",
       "Presented completed features and system updates during client demos, effectively communicating technical solutions and progress.",
-      "Collaborated with teammates to strategize and plan approaches for implementing user stories and meeting client requirements."
-    ]
+      "Collaborated with teammates to strategize and plan approaches for implementing user stories and meeting client requirements.",
+    ],
   },
   {
     title: "Software Developer",
     company: "FullScale Inc.",
     location: "Cebu City, Cebu, Philippines",
     period: "June 2018 to September 2019",
-    technologies: ".NET Framework 4.5, Azure Service Bus, Knockout JS, Azure App Service, Azure SQL, Elastic Search, Redis, Retrace APM, Jira, Git, Bitbucket",
+    technologies:
+      ".NET Framework 4.5, Azure Service Bus, Knockout JS, Azure App Service, Azure SQL, Elastic Search, Redis, Retrace APM, Jira, Git, Bitbucket",
     responsibilities: [
       "Modified and optimized existing platform code to fix coding errors, upgrade user interfaces, and enhance overall system performance.",
       "Developed and implemented bug fixes and new features to improve the functionality of the platform side of the SaaS product.",
@@ -58,8 +60,8 @@ export const experience: Experience[] = [
       "Documented service outages and issues by filling out detailed incident reports for future analysis and improvement.",
       "Balanced database pools in Azure to reduce SQL server load and enhance service stability.",
       "Submitted bug reports and investigation tickets to track and address software issues, contributing to ongoing service improvements.",
-      "Collaborated with teammates to enhance supportability and identify performance bottlenecks, driving continuous optimization efforts."
-    ]
+      "Collaborated with teammates to enhance supportability and identify performance bottlenecks, driving continuous optimization efforts.",
+    ],
   },
   {
     title: "Implementation Specialist",
@@ -72,22 +74,23 @@ export const experience: Experience[] = [
       "Performed load testing of the application in various test environments to ensure scalability and optimal performance.",
       "Configured promotions in the loyalty software according to the client's specifications, ensuring flexibility and accuracy in campaign execution.",
       "Tailored the solution to meet client needs by focusing on industry-specific workflows, client base, and operational protocols.",
-      "Collaborated closely with business analysts, development teams, and infrastructure specialists to design and deliver high-availability solutions for mission-critical applications."
-    ]
+      "Collaborated closely with business analysts, development teams, and infrastructure specialists to design and deliver high-availability solutions for mission-critical applications.",
+    ],
   },
   {
     title: "Software Engineer",
     company: "CMIL",
     location: "Cebu City, Cebu, Philippines",
     period: "June 2016 to September 2017",
-    technologies: "AngularJS, Java, Spring Boot, Jira, Jenkins, Swagger UI, Git, Bitbucket, SourceTree",
+    technologies:
+      "AngularJS, Java, Spring Boot, Jira, Jenkins, Swagger UI, Git, Bitbucket, SourceTree",
     responsibilities: [
       "Collaborated closely with software development and testing teams to design and develop data privacy solutions that met client functional requirements and compliance standards.",
       "Partnered with project managers, developers, and quality assurance teams to resolve technical issues and ensure smooth project execution.",
       "Modified and optimized existing data privacy software, correcting coding errors, upgrading user interfaces, and improving overall software performance.",
       "Debugged and troubleshot custom applications for clients, providing quick and accurate solutions to technical issues.",
-      "Developed code fixes and enhancements to be included in future software releases and patches, ensuring ongoing software improvement."
-    ]
+      "Developed code fixes and enhancements to be included in future software releases and patches, ensuring ongoing software improvement.",
+    ],
   },
   {
     title: "Associate Software Engineer",
@@ -100,7 +103,7 @@ export const experience: Experience[] = [
       "Tested code functionality to ensure quality and accuracy before submitting it to the testing team.",
       "Addressed bug fixes and change requests, contributing to the ongoing improvement of the software system.",
       "Collaborated with developers and product owners to stay up-to-date on product features and ensure alignment with intended functionality.",
-      "Modified and created documentation for code changes to maintain accurate records and facilitate future development."
-    ]
-  }
+      "Modified and created documentation for code changes to maintain accurate records and facilitate future development.",
+    ],
+  },
 ];
